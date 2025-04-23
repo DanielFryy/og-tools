f868c4852d43710a79a07fa076ea9d2ef12e90c3
Daniel Freire <dgz1606@gmail.com>
2025-04-23 12:18:56 -0500

    fix(ads): fix ads script

--- src/components/AdSense/AdSense.tsx
+++ src/components/AdSense/AdSense.tsx
@@ -1,15 +1,12 @@
-import Script from "next/script";
-
 import { AdSenseProps as Props } from "./AdSense.types";
 
 const AdSense = (props: Props) => {
   const { publisherId } = props;
 
   return (
-    <Script
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${publisherId}`}
-      strategy="afterInteractive"
       crossOrigin="anonymous"
     />
   );
