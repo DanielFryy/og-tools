f678e995148147092e9130d6c6d86c5eb8b64745
Daniel Freire <dgz1606@gmail.com>
2025-05-13 13:33:54 -0500

    style(sidebar): add smooth transition to the main layout width

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -23,7 +23,7 @@ const AppLayout = (props: Props) => {
           <Header onSettingsClick={() => setSettingsOpen(true)} />
           <main
             className={twMerge(
-              "flex-1 p-4 md:p-6 flex overflow-auto @container/main",
+              "flex-1 p-4 md:p-6 flex overflow-auto @container/main transition-[max-width] duration-200 ease-linear",
               open ? "max-w-[calc(100svw-var(--sidebar-width))]" : "max-w-[calc(100svw-var(--sidebar-width-icon))]"
             )}
           >
